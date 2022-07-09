596e48f3d28e98948c0319767701cca66e57078f
Stefan Peterson <stefanbpeterson@gmail.com>
2022-07-08 22:23:33 -0600

    code optimization

--- src/App.js
+++ src/App.js
@@ -41,6 +41,13 @@ class App extends Component {
   //   }
   // }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase()
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+
 
   render() {
     console.log('render')
@@ -53,12 +60,7 @@ class App extends Component {
       <div className="App">
         <div className='flex justify-center'>
           <div className='basis-1/3'>
-            <input className='search-box block w-full px-3 py-1.5 text-base text-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none' type='search' placeholder='Search Sonic Characters' onChange={(event) => {
-              const searchField = event.target.value.toLocaleLowerCase()
-              this.setState(() => {
-                return { searchField }
-              })
-            }} />
+            <input className='search-box block w-full px-3 py-1.5 text-base text-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none' type='search' placeholder='Search Sonic Characters' onChange={this.onSearchChange} />
           </div>
         </div>
         <div>
